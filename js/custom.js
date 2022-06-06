effd36e110f8c6b241649bf2ba3989f2a2ce8e40
unknown <you@example.com>
2022-06-06 21:24:21 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //menu peralax
+    $('.menu-parallax').parallax({imageSrc: 'images/menu-back.png'});
+
+    //master parallax
+    $('.master-parallax').parallax({imageSrc: 'images/master-back.png'});
+});
